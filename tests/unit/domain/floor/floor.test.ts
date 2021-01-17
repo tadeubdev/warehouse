4c28e24f57987a8819d25adebb5faacdc4341407
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-01-17 18:30:24 -0300

    test:  fix floor instance creation

--- tests/unit/domain/floor/floor.test.ts
+++ tests/unit/domain/floor/floor.test.ts
@@ -1,19 +1,27 @@
 import {Floor} from "../../../../src/domain/floor/Floor";
+import {DimensionAttrs} from "../../../../src/domain/floor/DimensionBase";
+import {SquareDimension} from "../../../../src/domain/floor/implements/SquareDimension";
 
 describe("Floor creation", () => {
 
     it('should create an Floor', function () {
-        const floor = new Floor(200, 200);
+        const dimensionAttrs: DimensionAttrs = {width: 200, height: 200};
+        const dimension = new SquareDimension(dimensionAttrs);
+        const floor = new Floor(dimension);
         expect(floor).toBeInstanceOf(Floor);
     });
 
     it('should throw an error when pass invalid dimension', function () {
         expect(() => {
-            new Floor(-200, -400);
+            const dimensionAttrs: DimensionAttrs = {width: -200, height: -400};
+            const dimension = new SquareDimension(dimensionAttrs);
+            new Floor(dimension);
         }).toThrow(Error);
 
         expect(() => {
-            new Floor(0, 0);
+            const dimensionAttrs: DimensionAttrs = {width: 0, height: 0};
+            const dimension = new SquareDimension(dimensionAttrs);
+            new Floor(dimension);
         }).toThrow(Error);
     });
 
