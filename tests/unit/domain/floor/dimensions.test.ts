b26befe0d41f204ee32e020a4b36e187deb55e38
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-01-17 18:28:07 -0300

    test:  fix dimension attrs creation on test

--- tests/unit/domain/floor/dimensions.test.ts
+++ tests/unit/domain/floor/dimensions.test.ts
@@ -1,24 +1,29 @@
 import {SquareDimension} from "../../../../src/domain/floor/implements/SquareDimension";
+import {DimensionAttrs, DimensionBase} from "../../../../src/domain/floor/DimensionBase";
 
 describe("Dimension creation", () => {
 
     it('should create an dimension', function () {
-        const dimension = new SquareDimension(100, 100);
+        const dimensionAttrs: DimensionAttrs = {width: 100, height: 100};
+        const dimension = new SquareDimension(dimensionAttrs);
         expect(dimension).toBeInstanceOf(SquareDimension);
     });
 
     it('should retriever an error on invalid dimension', function () {
         expect(() => {
-            new SquareDimension(-100, -100);
+            const dimensionAttrs: DimensionAttrs = {width: -100, height: -100};
+            new SquareDimension(dimensionAttrs);
         }).toThrow(Error);
 
         expect(() => {
-            new SquareDimension(0, 0);
+            const dimensionAttrs: DimensionAttrs = {width: 0, height: 0};
+            new SquareDimension(dimensionAttrs);
         }).toThrow(Error);
     });
 
     it('should retriever correctly dimension values', function () {
-        const dimension = new SquareDimension(100, 100);
+        const dimensionAttrs: DimensionAttrs = {width: 100, height: 100};
+        const dimension = new SquareDimension(dimensionAttrs);
 
         expect(dimension.dimension().width).toBe(100);
         expect(dimension.dimension().height).toBe(100);
