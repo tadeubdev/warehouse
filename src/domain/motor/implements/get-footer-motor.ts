c11c2e085ecdb3b1d61b29d6a7bd42dbd38bb360
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-08-28 08:43:21 -0300

    refactor: add generateIdHandler missing on constructor

--- src/domain/motor/implements/get-footer-motor.ts
+++ src/domain/motor/implements/get-footer-motor.ts
@@ -1,11 +1,12 @@
+import { IGenerateId } from "../../../infra/generate-id/igenerate-id";
 import {Position} from "../../worker/position";
 import {Size} from "../../worker/size";
 import {MotorBase} from "../motor-base";
 
 class GetFooterMotor extends MotorBase
 {
-    constructor(position: Position, size: Size) {
-        super(position, size);
+    constructor(position: Position, size: Size, generateIdHandler: IGenerateId) {
+        super(position, size, generateIdHandler);
     }
 
     move(): void {
