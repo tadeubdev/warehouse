bf298a913e2070b721c3d3dd36a04551052ab5d0
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-08-28 10:55:20 -0300

    fix: fix bot move-around-once

--- dist/src/domain/motor/implements/move-arround-once-motor.js
+++ dist/src/domain/motor/implements/move-arround-once-motor.js
@@ -6,7 +6,6 @@ class MoveArroundOnceMotor extends motor_base_1.MotorBase {
     constructor(position, size, generateIdHandler) {
         super(position, size, generateIdHandler);
         this.moved = {
-            first: false,
             right: false,
             bottom: false,
             left: false,
@@ -18,37 +17,24 @@ class MoveArroundOnceMotor extends motor_base_1.MotorBase {
         return this.movedArround === false;
     }
     move() {
-        if (this.moved.first === false) {
-            this._position.right(this._size.width);
-            this.moved.first = true;
-            return;
-        }
-        if (this._position.canMoveRight() === false && this.moved.right === false) {
-            this.moved.right = true;
-        }
-        if (this._position.canMoveBottom() === false && this.moved.bottom === false) {
-            this.moved.bottom = true;
-        }
-        if (this._position.canMoveRight() === false && this.moved.right === false) {
-            this.moved.right = true;
-        }
-        if (this._position.canMoveTop() === false && this.moved.top === false) {
-            this.moved.top = true;
-        }
         if (this._position.canMoveRight() && this.moved.right === false) {
             this._position.right(this._size.width);
+            this.moved.right = this._position.canMoveRight() === false;
             return;
         }
         if (this._position.canMoveBottom() && this.moved.bottom === false) {
             this._position.bottom(this._size.height);
+            this.moved.bottom = this._position.canMoveBottom() === false;
             return;
         }
         if (this._position.canMoveLeft() && this.moved.left === false) {
             this._position.left(this._size.width);
+            this.moved.left = this._position.canMoveLeft() === false;
             return;
         }
         if (this._position.canMoveTop() && this.moved.top === false) {
             this._position.top(this._size.height);
+            this.moved.top = this._position.canMoveTop() === false;
             return;
         }
         this.movedArround = true;
