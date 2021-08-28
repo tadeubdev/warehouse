8be2cf4f598416f3b74de0c088e3a9c3f41cef3b
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-08-28 09:02:08 -0300

    refactor: configure imports for playground

--- playground/footer-menu-implementation.js
+++ playground/footer-menu-implementation.js
@@ -1,19 +1,19 @@
-const { SquareDimension } = require("../src/domain/floor/implements/square-dimension");
-const { GetFooterMotor } = require("../src/domain/motor/implements/get-footer-motor");
-const { Position } = require("../src/domain/worker/position");
-const { Size } = require("../src/domain/worker/size");
-const { TimestampId } = require("../src/infra/generate-id/implements/timestamp-id");
+var Size = require("../dist/src/domain/worker/size").Size;
+var SquareDimension = require("../dist/src/domain/floor/implements/square-dimension").SquareDimension;
+var Position = require("../dist/src/domain/worker/position").Position;
+var TimestampId = require("../dist/src/infra/generate-id/implements/timestamp-id").TimestampId;
+var GetFooterMotor = require("../dist/src/domain/motor/implements/get-footer-motor").GetFooterMotor;
 
-const sizeAttrs = {width: 10, height: 10};
-const size = new Size(sizeAttrs);
+var sizeAttrs = {width: 10, height: 10};
+var size = new Size(sizeAttrs);
 
-const positionAttrs = {top: 0, left: 0};
-const dimensionAttrs = {width: 100, height: 100};
-const dimension = new SquareDimension(dimensionAttrs);
-const position = new Position(positionAttrs, dimension);
-const generateId = new TimestampId();
+var positionAttrs = {top: 0, left: 0};
+var dimensionAttrs = {width: 100, height: 100};
+var dimension = new SquareDimension(dimensionAttrs);
+var position = new Position(positionAttrs, dimension);
+var generateId = new TimestampId();
 
-const motor = new GetFooterMotor(position, size, generateId);
+var motor = new GetFooterMotor(position, size, generateId);
 
 while (motor.canMoveBottom()) {
     motor.move();
